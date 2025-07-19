a2b17f6401287bb7ba4192c13145d5dda0486b98
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-07-19 15:49:23 -0500

    generate computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log("hello");
+function getComputerChoice() {
+    // Generate random number from 0-2 to be assigned to rock, paper, or scissors
+    const intChoice = getRandInt(3);
+    
+    // Convert intChoice to string value
+    const strChoice = convertToStr(intChoice);
+
+    return strChoice;
+}
+
+// Generates random integer with Math.random()
+function getRandInt(intRange) {
+    return Math.floor(Math.random() * intRange);
+}
+
+// Convert intChoice to rock, paper, scissor string value
+function convertToStr(intChoice) {
+    // Initialize strChoice
+    let strChoice = ""; 
+
+    switch (intChoice) {
+        case 0:
+            strChoice = "rock";
+            break;
+        case 1:
+            strChoice = "paper";
+            break;
+        case 2:
+            strChoice = "scissors";            
+    }
+
+    return strChoice;
+}
+
+console.log(getComputerChoice());
